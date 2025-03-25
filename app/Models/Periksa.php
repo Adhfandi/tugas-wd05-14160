771b63bf708f5a4202ac4804a4aa44e29fcd30ba
Adhfandi <adhfandi@gmail.com>
2025-03-25 23:03:44 +0700

    New Repository & adding ERD

---
+++ app/Models/Periksa.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Periksa extends Model
+{
+    protected $table = 'periksa';
+     protected $fillable = [
+         'id_pasien',
+         'id_dokter',
+         'tgl_periksa',
+         'catatan',
+         'biaya_periksa'
+     ];
+ 
+     public function dokter(){
+         return $this->belongsTo(User::class, 'id_dokter');
+     }
+ 
+     public function pasien(){
+         return $this->belongsTo(User::class,'id_pasien');
+     }
+}
